63c1630dc8698a2edcb4fe7c73a516cccd6ab0e9
Binney <sar.binney@hotmail.co.uk>
2023-02-18 11:49:22 +0000

    add gridlines

--- src/routes/Map.tsx
+++ src/routes/Map.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Stage, Circle, Layer, Rect, Arrow } from "react-konva";
+import { Stage, Circle, Layer, Rect, Arrow, Line } from "react-konva";
 import testMap from './stories/maps/testMap.jpg';
 
 interface MapProps {
@@ -52,6 +52,14 @@ export default function Map(props: MapProps) {
         return (pos - min) * actualSize / mapSize;
     }
 
+    function normaliseX(pos: number) {
+        return normalise(pos, props.westEdge, mapWidth, actualWidth);
+    }
+
+    function normaliseY(pos: number) {
+        return actualHeight - normalise(pos, props.southEdge, mapHeight, actualHeight);
+    }
+
     navigator.geolocation.watchPosition((position) => {
         console.log("Setting to " + JSON.stringify(position));
         setLat(position.coords.latitude);
@@ -65,10 +73,12 @@ export default function Map(props: MapProps) {
         <Stage width={actualWidth} height={actualHeight}>
             <Layer>
                 <Rect x={0} y={0} width={actualWidth} height={actualHeight} fill={'grey'}></Rect>
+                {Array.from({ length: 10 }).map((_, i) => <Line stroke='green' strokeWidth={1} points={[0, i * 100, actualWidth, i * 100]}/>)}
+                {Array.from({ length: 10 }).map((_, i) => <Line stroke='green' strokeWidth={1} points={[i * 100, 0, i * 100, actualHeight]}/>)}
             </Layer>
             <Layer>
-                <Circle x={normalise(lon, props.westEdge, mapWidth, actualWidth)}
-                    y={actualHeight - normalise(lat, props.southEdge, mapHeight, actualHeight)}
+                <Circle x={normaliseX(lon)}
+                    y={normaliseY(lat)}
                     fill={"red"} stroke={'black'} radius={10} strokeWidth={2}></Circle>
                     <OffscreenArrow towardsX={normalise(lon, props.westEdge, mapWidth, actualWidth)}
                         towardsY={actualHeight - normalise(lat, props.southEdge, mapHeight, actualHeight)} />
